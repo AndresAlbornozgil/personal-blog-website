fa5df64dab7afd09e0ec710ba75221d6b2311cff
AA <aalborgil002@gmail.com>
2024-05-09 15:33:11 -0400

    Reordered content across files

---
+++ assets/js/form.js
@@ -0,0 +1,27 @@
+let darkMode = false; // Initially, light mode is active
+
+function toggleMode() {
+    const body = document.querySelector('body');
+    const button = document.querySelector('#screen-mode');
+
+    if (darkMode) {
+        // Switch to light mode
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        document.querySelector('#screen-mode').style.backgroundColor = "gold"
+        document.querySelector('#screen-mode').style.color = "black"
+        document.querySelector('#submit').style.backgroundColor = "black"
+    } else {
+        // Switch to dark mode
+        document.querySelector('body').style.backgroundColor = "black"
+        document.querySelector('body').style.color = "white"
+        document.querySelector('#submit').style.backgroundColor = "indigo"
+        document.querySelector('#screen-mode').style.backgroundColor = "indigo"
+        document.querySelector('#screen-mode').style.color = "white"
+    }
+
+    // Toggle the mode
+    darkMode = !darkMode;
+    }
+
+document.querySelector('#screen-mode').addEventListener('click', toggleMode);
