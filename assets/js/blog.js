a1d141c04e99845d4849685baf677887678883ed
AA <aalborgil002@gmail.com>
2024-05-09 13:42:26 -0400

    Added toggle dark/light functionality

---
+++ assets/js/blog.js
@@ -0,0 +1,29 @@
+let darkMode = false; // Initially, light mode is active
+
+function toggleMode() {
+    const body = document.querySelector('body');
+
+    // Toggle the mode
+    darkMode = !darkMode;
+
+    if (darkMode) {
+        // Switch to dark mode
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        document.querySelector('#screen-mode').style.backgroundColor = "indigo";
+        document.querySelector('#screen-mode').style.color = "white";
+        document.querySelector('#back-button').style.backgroundColor = "white";
+        document.querySelector('#back-button').style.color = "black";
+    } else {
+        // Switch to light mode
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        document.querySelector('#screen-mode').style.backgroundColor = "gold";
+        document.querySelector('#screen-mode').style.color = "black";
+        document.querySelector('#back-button').style.backgroundColor = "black";
+        document.querySelector('#back-button').style.color = "white";
+    }
+
+}
+
+document.querySelector('#screen-mode').addEventListener('click', toggleMode);
